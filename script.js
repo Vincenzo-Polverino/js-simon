b6e9fbd09090aae4573de7e9fa25b5af5ba9c860
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-10 15:31:57 +0200

    add random numbers

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('linked');
+const numberEl = document.querySelector('.numbers')
+const timerEl = document.querySelector('.timer')
+
+
+let numbers = '';
+for (let i = 0; i < 5; i++) {
+  numbers += Math.floor((Math.random() * 5) + 1) + ' ';
+}
+
+numberEl.innerHTML = `<h1>${numbers}</h1>`;
