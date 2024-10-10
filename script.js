639d911bc3c004a688dc75e79ab2b047696d2d6d
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-10 16:43:13 +0200

    add timer

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 console.log('linked');
-const numberEl = document.querySelector('.numbers')
-const timerEl = document.querySelector('.timer')
 
 
-let numbers = '';
+const numberEl = document.querySelector('.numbers');
+const inputEl = document.querySelectorAll('.input-number');
+const inputs_containerEl = document.querySelector('.inputs_container');
+const timerEl = document.querySelector('.timer');
+const checkEl = document.querySelector('.btn')
+
+let numbers = [];
 for (let i = 0; i < 5; i++) {
-  numbers += Math.floor((Math.random() * 5) + 1) + ' ';
+    numbers.push(Math.floor((Math.random() * 5) + 1));
 }
 
-numberEl.innerHTML = `<h1>${numbers}</h1>`;
+numberEl.innerHTML = `<h1>${numbers.join(' ')}</h1>`;
+
+let time_left = 30;
+timerEl.innerHTML = `<h1>${time_left}</h1>`;
+
+function countdown() {
+    time_left--;
+    timerEl.innerHTML = `<h1>${time_left}</h1>`;
+    if (time_left <= 0) {
+        clearInterval(clock);
+        numberEl.style.display = 'none';
+        inputs_containerEl.style.display = 'flex';
+        timerEl.style.display = 'none';
+    }
+}
+
+const clock = setInterval(countdown, 1000); 
